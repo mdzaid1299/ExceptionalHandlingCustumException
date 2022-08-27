ed3a3d4ad1a4d3837fb419d9dd7592923f273711
HP <mdzaid1299@gmail.com>
2022-08-28 01:34:22 +0530

    New Class added

--- src/Play.java
+++ src/Play.java
@@ -1,3 +1,19 @@
 public class Play {
+    public static void playAge(int a, int b) throws MyFirstException {
+        int mul = a * b;
+        if (mul < 100) {
+            throw new MyFirstException("Error Aa Gai");
+        } else {
+            System.out.println(mul);
+        }
+    }
 
+    public static void main(String[] args) {
+        Play play = new Play();
+        try {
+            play.playAge(100,3);
+        } catch (MyFirstException e) {
+           e.printStackTrace();
+        }
+    }
 }
